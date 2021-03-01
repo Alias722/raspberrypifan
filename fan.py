3908148e877ef98df29033328ff4e78678136712
alias722 <maxchengboy@gmail.com>
2021-03-01 23:58:00 +0800

    counting

--- fan.py
+++ fan.py
@@ -4,18 +4,28 @@ import time
 import RPi.GPIO as GPIO
 
 #init
+GPIO.cleanup()
+FAN_PIN = 40
 
-#FAN_PIN = 3
+#GPIO.setmode(GPIO.BCM)  #Board num
+GPIO.setmode(GPIO.BOARD) #Pin num
 
-GPIO.setmode(GPIO.BCM)  #Board num
-#GPIO.setmode(GPIO.BOARD) #Pin num
-
-GPIO.setup(3,GPIO.OUT)
+GPIO.setup(FAN_PIN,GPIO.OUT)
 #excute
 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
 #loop
+time1 = int(time.strftime("%S",time.localtime()))
 
-while True:
-    GPIO.output(3,GPIO.HIGH)
+print("time1 is : {}".format(time1))
+input("Hello")
+time2 = int(time.strftime("%S",time.localtime()))
 
+while time2-time1 < 10:
+    if time1 > time2:
+        time1 -= 60
+    time2 = int(time.strftime("%S",time.localtime()))
+    print(time2-time1)
+
+print("Finished")
+GPIO.cleanup()
